0cadfb0ea08fcefacb152ab5d208f5fbcc2af9d6
Azamat <azicus.sdu@gmail.com>
2024-01-09 02:46:44 +0600

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First line");
         System.out.println("Second line");
+        System.out.println("asdassd");
     }
 }
